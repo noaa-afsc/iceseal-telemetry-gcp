9485c7b5b2b2b81897bbd9d9ba0f5509839d3177
Josh M. London <josh.london@noaa.gov>
2024-06-07 14:11:34 -0700

    initial commit of code with parquet data

---
+++ R/get_locs_obs.R
@@ -0,0 +1,42 @@
+library(DBI)
+library(dbplyr)
+library(dplyr)
+library(RPostgres)
+library(sf)
+library(arrow)
+library(geoarrow)
+
+tryCatch({
+  con <- dbConnect(
+    RPostgres::Postgres(),
+    dbname = 'pep',
+    host = Sys.getenv('PEP_PG_IP'),
+    user = keyringr::get_kc_account("pgpep_londonj"),
+    password = keyringr::decrypt_kc_pw("pgpep_londonj")
+  )
+}, error = function(cond) {
+  print("Unable to connect to Database.")
+})
+
+tbl_deploy <- dplyr::tbl(con,in_schema("telem","tbl_tag_deployments")) |> 
+  dplyr::collect()
+
+locs_obs <- sf::st_read(con,Id("telem","geo_wc_locs_qa"))  |> 
+  dplyr::left_join(tbl_deploy) |> 
+  dplyr::filter(meta_project %in% c('Ice Seals'),
+                species %in% c('Hf','Pl')) |> 
+  dplyr::select(speno,deployid,ptt,instr,tag_family,type,quality,locs_dt,latitude,longitude,
+                error_radius, error_semi_major_axis,error_semi_minor_axis,
+                error_ellipse_orientation, project, species, qa_status,deploy_dt, end_dt,
+                deploy_lat,deploy_long,capture_lat,capture_long, geom)
+
+unlink(here::here('locs_obs'), 
+       recursive = TRUE, 
+       force = TRUE)
+
+write_dataset(locs_obs,
+              path='locs_obs',
+              format="parquet",
+              partitioning = 'speno')
+
+dbDisconnect(con, disconnect = TRUE)
