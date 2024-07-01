77edfdde2f8eeb77b679bc30228801c1daa6ff33
Josh M. London <josh.london@noaa.gov>
2024-07-01 14:34:22 -0700

    add age & sex to tbl_deploy

--- R/get_locs_obs.R
+++ R/get_locs_obs.R
@@ -18,9 +18,12 @@ tryCatch({
   print("Unable to connect to Database.")
 })
 
+tbl_speno <- dplyr::tbl(con,in_schema("capture","for_telem"))
+
 tbl_deploy <- dplyr::tbl(con,in_schema("telem","tbl_tag_deployments")) |> 
   dplyr::filter(meta_project %in% c('Ice Seals'),
                 species %in% c('Hf','Pl')) |> 
+  dplyr::left_join(tbl_speno) |> 
   dplyr::collect()
 
 unlink(here::here('tbl_deploy'), 
@@ -35,7 +38,7 @@ locs_obs <- sf::st_read(con,Id("telem","geo_wc_locs_qa"))  |>
   dplyr::left_join(tbl_deploy) |> 
   dplyr::select(speno,deployid,ptt,instr,tag_family,type,quality,locs_dt,latitude,longitude,
                 error_radius, error_semi_major_axis,error_semi_minor_axis,
-                error_ellipse_orientation, project, species, qa_status,deploy_dt, end_dt,
+                error_ellipse_orientation, project, species, age, sex, qa_status,deploy_dt, end_dt,
                 deploy_lat,deploy_long,capture_lat,capture_long, geom)
 
 unlink(here::here('locs_obs'), 
